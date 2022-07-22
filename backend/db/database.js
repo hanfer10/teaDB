5cd95506e49b849609877e51d510f96c9f9578e6
Zshawn Syed <zsyed91@gmail.com>
2022-07-22 11:19:47 -0500

    Migrate to new tech stack, remove existing code

--- backend/db/database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-
-const db = new Sequelize(`postgres://localhost:5432/teaDB`, {
-  logging: false,
-});
-
-module.exports = db;
--- backend/db/index.js
+++
@@ -1,9 +0,0 @@
-const db = require('./database');
-const Tea = require('./models/Tea');
-const User = require('./models/User');
-
-module.exports = {
-  db,
-  Tea,
-  User
-};

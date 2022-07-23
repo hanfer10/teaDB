8b1fd843b908c60dfa702914feee9835a6a97adf
Zshawn Syed <zsyed91@gmail.com>
2022-07-23 13:18:21 -0500

    Set root route Add fallback route for react-router to handle

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+	root to: 'root#index'
+  # Send default to home route for react-router
+  get '*path', to: 'root#index', constraints: -> (request) do
+    !request.xhr? && request.format.html?
+  end
 end

7d5e10bd30f89117bda1ab7b65e06da600509a77
Ferhan Syed <40723910+hanfer10@users.noreply.github.com>
2022-08-01 23:19:10 -0500

    Add userSlice and sign in and register thunks. (#10)
    * Add signin thunk
    * Add userSlice
    * Add register thunk
    * Add utilities file and make small corrections to userSlice

---
+++ app/javascript/redux/userSlice.ts
@@ -0,0 +1,48 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { withCsrf } from './utilities';
+
+export const signin = createAsyncThunk('users/signin', async (params) => {
+  const data = withCsrf();
+  data.user = {
+    username: params.username,
+    password: params.password
+  }
+
+  const response = await axios.post('/users/sign_in', data);
+  if (response.status != 200) {
+    console.log('authentication failed');
+    return undefined;
+  }
+  return response.data;
+})
+
+export const register = createAsyncThunk('users/register', async (params) => {
+  const data = withCsrf();
+  data.user = {
+    username: params.username,
+    password: params.password,
+    email: params.email
+  }
+
+  const response = await axios.post('/users', data);
+  return response.data;
+})
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user: undefined,
+    status: 'idle'
+  },
+  reducers: {
+    setUser: (state, action) => action.payload
+  },
+  extraReducers: {
+
+  },
+})
+
+export const { setUser } = userSlice.actions;
+
+export default userSlice.reducer;
